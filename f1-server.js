44a94b37b00a4dab4dd60b7c554418a1e157b8f9
Afilv222 <55000122+Afilv222@users.noreply.github.com>
2024-02-21 23:48:04 -0700

    Starting the node assign

---
+++ f1-server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const supa = require('@supabase/supabase-js');
+const app = express();
+
+
+app.listen(8080, () => {
+        console.log('listening on port 8080');
+} 
+)
