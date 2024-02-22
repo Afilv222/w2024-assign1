f1868aadfae87a9c35eca7225da0d397094c610a
Afilv222 <55000122+Afilv222@users.noreply.github.com>
2024-02-22 01:13:38 -0700

    Wokring on the third query

--- f1-server.js
+++ f1-server.js
@@ -3,6 +3,47 @@ const supa = require('@supabase/supabase-js');
 const app = express();
 
 
+
+const supaUrl = process.env.supaUrl;
+const supaAnonKey = process.env.supaAnonKey;
+
+const supabase = supa.createClient(supaUrl, supaAnonKey);
+
+
+
+app.get('/api/seasons', async (req, res) => {
+    const {data, error} = await supabase
+    .from('seasons')
+    .select('*')
+    ;
+
+    if (error) {
+        console.error('Error fetching data:', error.message);
+        return;
+    }
+
+    res.send(data);
+});
+
+app.get('/api/circuits', async (req, res) => {
+    const {data, error} = await supabase
+    .from('circuits')
+    .select('*')
+    ;
+
+    if (error) {
+        console.error('Error fetching data:', error.message);
+        return;
+    }
+
+    res.send(data);
+});
+
+
+
+
+
+
 app.listen(8080, () => {
         console.log('listening on port 8080');
 } 
