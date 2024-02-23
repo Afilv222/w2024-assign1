7f9584d28812cd0d856b3c17caea124813b1501c
Afilv222 <55000122+Afilv222@users.noreply.github.com>
2024-02-22 19:54:54 -0700

    Worked on few more queries

--- f1-server.js
+++ f1-server.js
@@ -39,6 +39,213 @@ app.get('/api/circuits', async (req, res) => {
     res.send(data);
 });
 
+app.get('/api/circuits/:ref', async (req, res) => {
+  try{
+    const {data, error} = await supabase
+    .from('circuits')
+    .select('*')
+    .eq('circuitRef',req.params.ref)
+    ;
+    
+    if (error) {
+        throw error; // Throw the error to be caught by the catch block
+    }
+    
+    if (!data || data.length === 0) {
+        return errorHandler(res, req.params.ref) 
+    }
+
+    res.send(data);
+  }catch{
+    res.json({ error: 'Server Error' });
+  }
+  
+});
+
+app.get('/api/circuits/season/:year', async (req, res) => {
+  try{
+      const {data, error} = await supabase
+      .from('races')
+      .select(`year,round, circuits(*)`)
+      .eq('year',req.params.year)
+      .order('round',{ ascending: true })
+    
+      
+    if (error) {
+        throw error;
+    }  
+      
+    if (!data || data.length === 0) {
+        return errorHandler(res, req.params.year)    
+    }
+
+    res.send(data);
+  }catch{
+    res.json({ error: 'Server Error' });
+  }
+});
+
+app.get('/api/constructors', async (req, res) => {
+  try{
+      const {data, error} = await supabase
+      .from('constructors')
+      .select('*')
+    
+      
+    if (error) {
+        throw error;
+    }  
+      
+    if (!data || data.length === 0) {
+        return errorHandler(res, req)    
+    }
+
+    res.send(data);
+  }catch{
+    res.json({ error: 'Server Error' });
+  }
+});
+
+
+app.get('/api/constructors/:ref', async (req, res) => {
+  try{
+      const {data, error} = await supabase
+      .from('constructors')
+      .select('*')
+      .eq('constructorRef',req.params.ref)
+    
+      
+    if (error) {
+        throw error;
+    }  
+      
+    if (!data || data.length === 0) {
+        return errorHandler(res, req.params.ref)    
+    }
+
+    res.send(data);
+  }catch{
+    res.json({ error: 'Server Error' });
+  }
+});
+
+app.get('/api/drivers', async (req, res) => {
+  try{
+      const {data, error} = await supabase
+      .from('drivers')
+      .select('*')
+    
+      
+    if (error) {
+        throw error;
+    }  
+      
+    if (!data || data.length === 0) {
+        return errorHandler(res, req)    
+    }
+
+    res.send(data);
+  }catch{
+    res.json({ error: 'Server Error' });
+  }
+});
+
+app.get('/api/drivers/:ref', async (req, res) => {
+  try{
+      const {data, error} = await supabase
+      .from('drivers')
+      .select('*')
+      .eq('driverRef',req.params.ref)
+    
+      
+    if (error) {
+        throw error;
+    }  
+      
+    if (!data || data.length === 0) {
+        return errorHandler(res, req.params.ref)    
+    }
+
+    res.send(data);
+  }catch{
+    res.json({ error: 'Server Error' });
+  }
+});
+
+app.get('/api/drivers/search/:substring', async (req, res) => {
+  try{
+      const {data, error} = await supabase
+      .from('drivers')
+      .select('*')
+      .ilike('surname', `${req.params.substring}%`)
+    
+      
+    if (error) {
+        throw error;
+    }  
+      
+    if (!data || data.length === 0) {
+        return errorHandler(res, req.params.substring)    
+    }
+
+    res.send(data);
+  }catch{
+    res.json({ error: 'Server Error' });
+  }
+});
+
+
+app.get('/api/drivers/race/:raceId', async (req, res) => {
+  try{
+      const {data, error} = await supabase
+      .from('qualifying')
+      .select('drivers(*)')
+      .eq('raceId',req.params.raceId)
+    
+      
+    if (error) {
+        throw error;
+    }  
+      
+    if (!data || data.length === 0) {
+        return errorHandler(res, req.params.raceId)    
+    }
+
+    res.send(data);
+  }catch{
+    res.json({ error: 'Server Error' });
+  }
+});
+
+app.get('/api/races/:raceId', async (req, res) => {
+  try{
+      const {data, error} = await supabase
+      .from('races')
+      .select('raceId,year,round,name,date,time,url,fp1_date,fp1_time,fp2_date,fp2_time,fp3_date,fp3_time,quali_date,quali_time,sprint_date,sprint_time,circuits(name,location,country)')
+      //.select('*')
+      .eq('raceId',req.params.raceId)
+    
+      
+    if (error) {
+        throw error;
+    }  
+      
+    if (!data || data.length === 0) {
+        return errorHandler(res, req.params.raceId)    
+    }
+
+    res.send(data);
+  }catch{
+    res.json({ error: 'Server Error' });
+  }
+});
+
+
+
+const errorHandler = (res,req) => {
+  return res.json({ error: `not found ${req}` }); 
+}
+
 
 
 
